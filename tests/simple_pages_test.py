6666b8b3254d5fe524ad64c09699371dc9e7e703
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-02-28 22:13:15 -0500

    test updated v3

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,7 +1,7 @@
 """This test the homepage"""
 
 ##def test_request_main_menu_links(client):
-    """This makes the index page"""
+##   """This makes the index page"""
 ##    response = client.get("/")
 ##    assert response.status_code == 200
 ##    assert b'<a class="nav-link" href="/about">About</a>' in response.data
@@ -20,7 +20,7 @@ def test_request_about(client):
     assert b"About" in response.data
 
 ## def test_request_page1(client):
-    """This makes the index page"""
+##  """This makes the index page"""
 ##    response = client.get("/index")
 ##    assert response.status_code == 200
 ##    assert b"Page 1" in response.data
