bb2e34ed95440bbf687b7953de4ac278df6cc0a7
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-03-07 21:58:30 -0500

    test updated

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,11 +1,15 @@
 """This test the homepage"""
 
-##def test_request_main_menu_links(client):
+def test_request_main_menu_links(client):
 ##   """This makes the index page"""
-##    response = client.get("/")
-##    assert response.status_code == 200
-##    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-##    assert b'<a class="nav-link" href="/index">Index</a>' in response.data
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'<a class="nav-link" href="/about">About</a>' in response.data
+    assert b'<a class="nav-link" href="/index">Index</a>' in response.data
+    assert b'<a class="nav-link" href="/github">Index</a>' in response.data
+    assert b'<a class="nav-link" href="/docker">Index</a>' in response.data
+    assert b'<a class="nav-link" href="/cicd">Index</a>' in response.data
+    assert b'<a class="nav-link" href="/pythonflask">Index</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
