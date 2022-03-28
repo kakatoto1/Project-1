bbca5258412623b69988d9a8dc2ad61aea9b4512
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-03-28 13:54:14 -0400

    update calculator

---
+++ calculator/Calculator.py
@@ -0,0 +1,22 @@
+class Calculator:
+    result = 0
+    data = []
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = round(division(a, b), 9)
+        return self.result
